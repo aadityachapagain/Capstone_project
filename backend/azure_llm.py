3fdfdd3941b96c57e3a5b99bc4121dc8824f77d7
Anish Pandey <anish.pandey@docsumo.com>
2024-03-26 12:32:01 +1100

    Add LLM to extract JSOn from transcript for mindmap

--- backend/azure_llm.py
+++ backend/azure_llm.py
@@ -3,23 +3,24 @@ import openai
 from loguru import logger
 from langchain_openai import AzureChatOpenAI
 from langchain.schema import HumanMessage
-from dotenv import load_dotenv
 
 
-# Load environment variables from .env file
-load_dotenv()
+class azureLLM:
+    def __init__(self, **kwargs):
+        self.model = AzureChatOpenAI(
+            openai_api_version=kwargs.get("api_version", "2023-07-01-preview"),
+            azure_deployment=kwargs.get("api_version", "gpt-4"),
+            model_name=kwargs.get("model_name", "gpt-4"),
+            azure_endpoint=kwargs.get(
+                "azure_endpoint", "https://allotrac-hackathon2024.openai.azure.com/"
+            ),
+            openai_api_key=kwargs.get("api_key"),
+            temperature=kwargs.get("temperature", 0),
+        )
+        self.prompt_path = kwargs.get("prompt_path", "./backend/prompt.txt")
 
-API_ENDPOINT = "https://allotrac-hackathon2024.openai.azure.com/"  # change this
-API_VERSION = "2023-07-01-preview"
-API_KEY = os.getenv("azurellm_key")
-
-
-model = AzureChatOpenAI(
-    openai_api_version=API_VERSION,
-    azure_deployment="gpt-4",
-    model_name="gpt-4",
-    azure_endpoint=API_ENDPOINT,
-    openai_api_key=API_KEY,
-    temperature=0,
-)
-logger.error(model([HumanMessage(content="Tell me an american Joke")]))
+    def extract_response(self, transcribe_data):
+        with open(self.prompt_path, "r") as prompt_file:
+            prompt_data = prompt_file.read()
+        final_prompt = transcribe_data + "\n" + prompt_data
+        return self.model([HumanMessage(content=final_prompt)]).content
